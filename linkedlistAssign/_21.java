edd9c0b720c33e2ac6d2e273ad70faba68f7caeb
Karunasahane <118338095+Karunasahane@users.noreply.github.com>
2024-01-08 15:22:43 +0530

    Update _21.java

--- linkedlistAssign/_21.java
+++ linkedlistAssign/_21.java
@@ -18,3 +18,30 @@ Output:1 1 2 4
 Explanation: After merging the given two linked lists , we have 1, 1, 2, 4 as output.
 Expected Time Complexity : O(n+m)
 Expected Auxiliary Space : O(1)*/
+
+public Node mergeTwoLists(Node list1, Node list2) {
+        Node t1=list1;
+        Node t2=list2;
+        Node n=new Node(1);
+        Node temp=n;
+        while(t1!=null && t2!=null){
+        if(t1.val<t2.val){
+            temp.next=t1;
+            temp=t1;
+            t1=t1.next;
+        }
+        else{
+            temp.next=t2;
+            temp=t2;
+            t2=t2.next;
+        }
+        }
+        if(t1!=null){
+            temp.next=t1;
+        }
+        if(t2!=null){
+            temp.next=t2;
+        }
+        return n.next;
+        
+    }
