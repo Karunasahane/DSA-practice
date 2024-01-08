186bcf5146c2f1f3a6df7f1b89b2eefbb2cc0f62
Karunasahane <118338095+Karunasahane@users.noreply.github.com>
2024-01-08 15:17:41 +0530

    Update _19.java

--- linkedlistAssign/_19.java
+++ linkedlistAssign/_19.java
@@ -3,30 +3,49 @@ Given two singly linked lists of size N and M, write a program to get the point 
 lists intersect each other.
 Example 1:
 Input:
-LinkList1 = 3->6->9->common
-LinkList2 = 10->common
-common = 15->30->NULL
-Output: 15
-Explanation:
-
-Example 2:
-Input:
 Linked List 1 = 4->1->common
 Linked List 2 = 5->6->1->common
 common = 8->4->5->NULL
 Output: 8
-Explanation:
-4 5
-| |
-1 6
-\ /
-8 ----- 1
-|
-4
-|
-5
-|
 
-NULL
 Expected Time Complexity: O(N+M)
 Expected Auxiliary Space: O(1)*/
+
+class InterPoint{
+    public int getIntersectionNode(Node headA, Node headB) {
+            Node ans=null;
+            int m;int n; Node temp1; Node temp2;
+            if(size(headA)>size(headB)){
+                n=size(headA);
+                m=size(headB);
+                temp1=headA;
+                temp2=headB;
+            }
+            else{
+                n=size(headB);
+                m=size(headA);
+                temp2=headA;
+                temp1=headB;
+            }
+                
+            for(int i=1;i<=n-m;i++){
+                temp1=temp1.next;
+            }
+
+            while(temp1!=temp2){
+                    temp1=temp1.next;
+                    temp2=temp2.next;
+            }
+        return temp1.data;
+    }
+
+    int size(Node head){
+        Node temp=head;
+        int count=0;
+        while(temp.next!=null){
+            count++;
+            temp=temp.next;
+        }
+        return count;
+    }
+}
